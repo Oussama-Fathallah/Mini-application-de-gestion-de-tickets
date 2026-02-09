918a3da0a6bd387dc93f117a555c3c8432b51ed3
Oussama Fathallah <oussama.fathallah05@gmail.com>
2026-02-09 13:56:39 +0100

    feat: configuration du routage et du client http

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ListeDemandesComponent } from './components/liste-demandes/liste-demandes.component';
+import { AjoutDemandeComponent } from './components/ajout-demande/ajout-demande.component';
+import { DetailsDemandeComponent } from './components/details-demande/details-demande.component';
+import { JournalActiviteComponent } from './components/journal-activite/journal-activite.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'demandes', pathMatch: 'full' },
+  { path: 'demandes', component: ListeDemandesComponent },
+  { path: 'ajouter', component: AjoutDemandeComponent },
+  { path: 'details/:id', component: DetailsDemandeComponent },
+  { path: 'journal', component: JournalActiviteComponent }
+];
